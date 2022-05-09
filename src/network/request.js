e1e7163dbb5519cda58639bd1d04a56e3e83cdac
AlohaXuuuu <65662075+Aloha-xu@users.noreply.github.com>
2022-05-09 09:47:40 +0800

    Update request.js

--- src/network/request.js
+++ src/network/request.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export function request(config){
     const instance=axios.create({
-    baseURL:'http://114.132.239.118:3000',
+    baseURL:'',
     timeout:5000,
     //解决了跨域问题 这个设置网站可以带cookie请求 
     withCredentials: true //关键
